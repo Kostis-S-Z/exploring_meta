03734f395fdabc27d6d52bd62f5bef345004d3ca
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-03-13 13:11:53 +0100

    Move algo.py to new dir

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from .data_pre import get_mini_imagenet, get_omniglot, prepare_batch
-from .algo import accuracy, fast_adapt
 from .cca import get_cca_similarity
 from .cka import get_linear_CKA, get_kernel_CKA
 from .cl_metrics import calc_cl_metrics
