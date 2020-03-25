5adc13b592bf65686294c244830bb9bf50b63f4c
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-03-25 14:02:19 +0100

    Add to test CL and rep exp

--- test_vision.py
+++ test_vision.py
@@ -11,7 +11,24 @@ from maml_vision import evaluate
 
 cuda = True
 
-base_path = "results/maml_min_23_03_16h08_1_1215"
+base_path = "results/reprod_results/maml_min_04_03_16h43_42_2373"
+
+eval_iters = False
+cl_exp = False
+rep_exp = True
+
+cl_params = {
+    "adapt_steps": 1,
+    "inner_lr": 0.1,
+    "n_tasks": 10
+}
+
+rep_params = {
+    "adapt_steps": 1,
+    "inner_lr": 0.1,
+    "n_tasks": 2,
+    "layers": [3, 4]
+}
 
 
 def run(path):
@@ -34,24 +51,47 @@ def run(path):
         else:
             model = l2l.vision.models.OmniglotFC(28 ** 2, params['ways'])
 
-    ckpnt = base_path + "/model_checkpoints/"
-    model_ckpnt_results = {}
+    # Evaluate the model at every checkpoint
+    if eval_iters:
+        ckpnt = base_path + "/model_checkpoints/"
+        model_ckpnt_results = {}
+        for model_ckpnt in os.scandir(ckpnt):
+            if model_ckpnt.path.endswith(".pt"):
+                print(f'Testing {model_ckpnt.path}')
+                res = evaluate_model(params, model, test_tasks, device, model_ckpnt.path)
+                model_ckpnt_results[model_ckpnt.path] = res
 
-    for model_ckpnt in os.scandir(ckpnt):
-        if model_ckpnt.path.endswith(".pt"):
-            print(f'Testing {model_ckpnt.path}')
-            res = evaluate_model(params, model, test_tasks, device, model_ckpnt.path)
-            model_ckpnt_results[model_ckpnt.path] = res
+        with open(base_path + '/ckpnt_results.json', 'w') as fp:
+            json.dump(model_ckpnt_results, fp, sort_keys=True, indent=4)
 
-    with open(base_path + '/ckpnt_results.json', 'w') as fp:
-        json.dump(model_ckpnt_results, fp, sort_keys=True, indent=4)
+    final_model = base_path + '/model.pt'
+    # Run a Continual Learning experiment
+    if cl_exp:
+        print("Running Continual Learning experiment...")
+        model.load_state_dict(torch.load(final_model))
+        model.to(device)
+        maml = l2l.algorithms.MAML(model, lr=cl_params['inner_lr'], first_order=False)
+        loss = torch.nn.CrossEntropyLoss(reduction='mean')
+
+        run_cl_exp(base_path, maml, loss, test_tasks, device,
+                   params['ways'], params['shots'], cl_params=cl_params)
+
+    # Run a Representation change experiment
+    if rep_exp:
+        model.load_state_dict(torch.load(final_model))
+        model.to(device)
+        maml = l2l.algorithms.MAML(model, lr=rep_params['inner_lr'], first_order=False)
+        loss = torch.nn.CrossEntropyLoss(reduction='mean')
+
+        print("Running Representation experiment...")
+        run_rep_exp(base_path, maml, loss, test_tasks, device,
+                    params['ways'], params['shots'], rep_params=rep_params)
 
 
 def evaluate_model(params, model, test_tasks, device, path):
-
     model.load_state_dict(torch.load(path))
     model.to(device)
-    maml = l2l.algorithms.MAML(model, lr=params['fast_lr'], first_order=False)
+    maml = l2l.algorithms.MAML(model, lr=params['inner_lr'], first_order=False)
     loss = torch.nn.CrossEntropyLoss(reduction='mean')
 
     return evaluate(params, test_tasks, maml, loss, device)
@@ -59,4 +99,3 @@ def evaluate_model(params, model, test_tasks, device, path):
 
 if __name__ == '__main__':
     run(base_path)
-
