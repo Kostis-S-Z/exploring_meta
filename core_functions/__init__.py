4be52ab0164976bc64ab1c5bacf3e12d105e5e9e
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-04-07 12:45:35 +0200

    Build CNN for progcen

--- core_functions/__init__.py
+++ core_functions/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 from .vision import fast_adapt, accuracy, prepare_batch
-from .policies import DiagNormalPolicy
+from .policies import DiagNormalPolicy, DiagNormalPolicyCNN
 from .rl import fast_adapt_a2c, meta_surrogate_loss, meta_optimize, evaluate
