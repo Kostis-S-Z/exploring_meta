ee61e4af1bd318ed55a09ea8aecdc914bad89b3e
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-05-13 15:56:05 +0200

    Clean up ANIL-TRPO implementation

--- anil_trpo.py
+++ anil_trpo.py
@@ -13,16 +13,15 @@ import cherry as ch
 import learn2learn as l2l
 
 from utils import *
-from core_functions.policies import ANILDiagNormalPolicy
-from core_functions.rl import fast_adapt_trpo_a2c, meta_optimize, evaluate
-from misc_scripts import run_cl_rl_exp
+from core_functions.policies import DiagNormalPolicyANIL
+from core_functions.rl import fast_adapt_trpo, meta_optimize_trpo, evaluate_trpo
 
 # ANIL Defaults: meta_batch_size: 40, adapt_steps: 1, adapt_batch_size: 20, inner_lr: 0.1
 # Train for 500 epochs then evaluate on a new set of tasks.
 
 params = {
     "outer_lr": 0.1,  # ?
-    "inner_lr": 0.2,  # ?
+    "inner_lr": 0.1,
     "tau": 1.0,
     "gamma": 0.99,
     "backtrack_factor": 0.5,  # Meta-optimizer
@@ -64,7 +63,7 @@ fc_neurons = 100
 #   - Particles2D-v1
 #   - AntDirection-v1
 env_name = "Particles2D-v1"
-workers = 2
+workers = 4
 
 cl_test = False
 rep_test = False
@@ -74,10 +73,10 @@ cuda = False
 wandb = False
 
 
-class AnilRL(Experiment):
+class AnilTRPO(Experiment):
 
     def __init__(self):
-        super(AnilRL, self).__init__("anil", env_name, params, path="rl_results/", use_wandb=wandb)
+        super(AnilTRPO, self).__init__("anil", env_name, params, path="rl/results/", use_wandb=wandb)
 
         def make_env():
             env = gym.make(env_name)
@@ -104,73 +103,52 @@ class AnilRL(Experiment):
     def run(self, env, device):
 
         baseline = ch.models.robotics.LinearValue(env.state_size, env.action_size)
-        baseline.to(device)
 
-        policy = ANILDiagNormalPolicy(env.state_size, env.action_size, fc_neurons, network)
-        features = policy.features
-        features.to(device)
-        head = policy.head
-        head.to(device)
-
+        policy = DiagNormalPolicyANIL(env.state_size, env.action_size, fc_neurons, network)
         policy = l2l.algorithms.MAML(policy, lr=self.params['inner_lr'])
 
-        all_parameters = list(features.parameters()) + list(head.parameters())
-        optimizer = torch.optim.Adam(all_parameters, lr=self.params['outer_lr'])
-
-        self.log_model(policy.features, device, input_shape=(1, env.state_size), name='features')
+        self.log_model(policy.body, device, input_shape=(1, env.state_size), name='body')
         self.log_model(policy.head, device, input_shape=(env.action_size, fc_neurons), name='head')
 
         t = trange(self.params['num_iterations'])
         try:
             for iteration in t:
 
-                optimizer.zero_grad()
-
-                iter_loss = 0.0
                 iter_reward = 0.0
                 iter_replays = []
                 iter_policies = []
 
                 task_list = env.sample_tasks(self.params['meta_batch_size'])
-                task_i = 0
-                for task in task_list:
-                    # Copy only the policy / head
-                    learner = policy.clone()
+                for task_i in trange(len(task_list), leave=False, desc="Task", position=0):
+                    task = task_list[task_i]
 
+                    clone = deepcopy(policy)
                     env.set_task(task)
                     env.reset()
                     task = ch.envs.Runner(env)
 
-                    # Adapt
-                    loss, task_replay, val_rew = fast_adapt_trpo_a2c(task, learner, baseline, self.params,
-                                                                     first_order=False, device=device)
+                    # Fast adapt
+                    learner, task_replay, task_rew = fast_adapt_trpo(task, clone, baseline, self.params, anil=True,
+                                                                     first_order=True, device=device)
 
-                    loss.backward()
-
-                    iter_loss = loss.item()
-                    iter_reward += val_rew
+                    iter_reward += task_rew
                     iter_replays.append(task_replay)
                     iter_policies.append(learner)
 
-                    print(f"Task {task_i}: Loss: {round(iter_loss, 4)} | Rew: {round(val_rew, 2)}")
-                    task_i += 1
+                # Meta-optimize
+                meta_optimize_trpo(self.params, policy, baseline, iter_replays, iter_policies, device)
 
+                # Log
                 adapt_reward = iter_reward / self.params['meta_batch_size']
-                iter_loss = iter_loss / self.params['meta_batch_size']
-                metrics = {'adapt_reward': adapt_reward,
-                           'loss': iter_loss}
+                metrics = {'adapt_reward': adapt_reward}
 
                 t.set_postfix(metrics)
                 self.log_metrics(metrics)
 
-                # Average the accumulated gradients and optimize
-                for p in all_parameters:
-                    p.grad.data.mul_(1.0 / self.params['meta_batch_size'])
-                optimizer.step()
-
                 if iteration % self.params['save_every'] == 0:
-                    self.save_model_checkpoint(policy.features, 'features_' + str(iteration + 1))
+                    self.save_model_checkpoint(policy.body, 'body_' + str(iteration + 1))
                     self.save_model_checkpoint(policy.head, 'head_' + str(iteration + 1))
+                    self.save_model_checkpoint(baseline, 'baseline_' + str(iteration + 1))
 
         # Support safely manually interrupt training
         except KeyboardInterrupt:
@@ -178,23 +156,19 @@ class AnilRL(Experiment):
             self.logger['manually_stopped'] = True
             self.params['num_iterations'] = iteration
 
-        self.save_model(policy.features)
-        self.save_model(policy.head)
+        self.save_model(policy.body, name="body")
+        self.save_model(policy.head, name="head")
+        self.save_model(baseline, name="baseline")
 
         self.logger['elapsed_time'] = str(round(t.format_dict['elapsed'], 2)) + ' sec'
         # Evaluate on new test tasks
-        # TODO: evaluation also needs changes
-        self.logger['test_reward'] = evaluate(env, policy, baseline, eval_params)
+        self.logger['test_reward'] = evaluate_trpo(env, policy, baseline, eval_params)
         self.log_metrics({'test_reward': self.logger['test_reward']})
         self.save_logs_to_file()
 
-        if cl_test:
-            print("Running Continual Learning experiment...")
-            run_cl_rl_exp(self.model_path, env, policy, baseline, cl_params=cl_params)
-
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ANIL on RL tasks')
+    parser = argparse.ArgumentParser(description='ANIL-TRPO on RL-MuJoCo tasks')
 
     parser.add_argument('--env', type=str, default=env_name, help='Pick an environment')
 
@@ -222,4 +196,4 @@ if __name__ == '__main__':
 
     params['seed'] = args.seed
 
-    AnilRL()
+    AnilTRPO()
