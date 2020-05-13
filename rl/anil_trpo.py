e4566f47fdd63a77dba655166b89e89560db9440
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-05-13 17:24:40 +0200

    ANIL-TRPO cleanup

--- rl/anil_trpo.py
+++ rl/anil_trpo.py
@@ -8,35 +8,34 @@ from copy import deepcopy
 
 from tqdm import trange
 
-import gym
 import cherry as ch
-import learn2learn as l2l
+from learn2learn.algorithms import MAML
 
 from utils import *
 from core_functions.policies import DiagNormalPolicyANIL
 from core_functions.rl import fast_adapt_trpo, meta_optimize_trpo, evaluate_trpo
 
-# ANIL Defaults: meta_batch_size: 40, adapt_steps: 1, adapt_batch_size: 20, inner_lr: 0.1
-# Train for 500 epochs then evaluate on a new set of tasks.
 
 params = {
-    "outer_lr": 0.1,  # ?
-    "inner_lr": 0.1,
-    "tau": 1.0,
-    "gamma": 0.99,
-    "backtrack_factor": 0.5,  # Meta-optimizer
-    "ls_max_steps": 15,  # Meta-optimizer
-    "max_kl": 0.01,  # Meta-optimizer
-    "adapt_batch_size": 20,  # "shots"
-    "meta_batch_size": 40,  # "ways"
-    "adapt_steps": 1,
-    "num_iterations": 500,
-    "save_every": 50,
-    "seed": 42}
-
-# Adapt steps: how many times you will replay & learn a specific number of episodes (=adapt_batch_size)
-# Meta_batch_size (=ways): how many tasks an epoch has. (a task can have one or many episodes)
-# Adapt_batch_size (=shots): number of episodes (not steps!) during adaptation
+    # Inner loop parameters
+    'inner_lr': 0.1,
+    'adapt_steps': 1,
+    'adapt_batch_size': 10,  # 'shots' (will be *evenly* distributed across workers)
+    # Outer loop parameters
+    'meta_batch_size': 20,  # 'ways'
+    'outer_lr': 0.1,
+    'backtrack_factor': 0.5,
+    'ls_max_steps': 15,
+    'max_kl': 0.01,
+    # Common parameters
+    'activation': 'tanh',  # for MetaWorld use tanh, others relu
+    'tau': 1.0,
+    'gamma': 0.99,
+    'fc_neurons': 100,
+    # Other parameters
+    'num_iterations': 1000,
+    'save_every': 25,
+    'seed': 42}
 
 eval_params = {
     'n_eval_adapt_steps': 5,  # Number of steps to adapt to a new task
@@ -47,28 +46,17 @@ eval_params = {
     'gamma': params['gamma'],
 }
 
-cl_params = {
-    "adapt_steps": 10,
-    "adapt_batch_size": 10,  # shots
-    "inner_lr": 0.3,
-    "gamma": 0.99,
-    "tau": 1.0,
-    "n_tasks": 5
-}
-
-network = [100, 100]
-fc_neurons = 100
-
 # Environments:
 #   - Particles2D-v1
 #   - AntDirection-v1
-env_name = "Particles2D-v1"
+#   - ML1_reach-v1, ML1_pick-place-v1, ML1_push-v1
+#   - ML10, ML45
+
+env_name = 'ML1_reach-v1'
 workers = 4
 
 cl_test = False
 rep_test = False
-
-cuda = False
 
 wandb = False
 
@@ -76,39 +64,25 @@ wandb = False
 class AnilTRPO(Experiment):
 
     def __init__(self):
-        super(AnilTRPO, self).__init__("anil", env_name, params, path="rl/results/", use_wandb=wandb)
-
-        def make_env():
-            env = gym.make(env_name)
-            env = ch.envs.ActionSpaceScaler(env)
-            return env
+        super(AnilTRPO, self).__init__("anil_trpo", env_name, params, path="rl/results/", use_wandb=wandb)
 
         device = torch.device('cpu')
-
         random.seed(self.params['seed'])
         np.random.seed(self.params['seed'])
         torch.manual_seed(self.params['seed'])
 
-        env = l2l.gym.AsyncVectorEnv([make_env for _ in range(workers)])
-        env.seed(self.params['seed'])
-        env.set_task(env.sample_tasks(1)[0])
-        env = ch.envs.Torch(env)
-
-        if cuda and torch.cuda.device_count():
-            torch.cuda.manual_seed(self.params['seed'])
-            device = torch.device('cuda')
-
+        env = make_env(env_name, workers, params['seed'])
         self.run(env, device)
 
     def run(self, env, device):
 
         baseline = ch.models.robotics.LinearValue(env.state_size, env.action_size)
 
-        policy = DiagNormalPolicyANIL(env.state_size, env.action_size, fc_neurons, network)
-        policy = l2l.algorithms.MAML(policy, lr=self.params['inner_lr'])
+        policy = DiagNormalPolicyANIL(env.state_size, env.action_size, params['fc_neurons'])
+        policy = MAML(policy, lr=self.params['inner_lr'])
 
         self.log_model(policy.body, device, input_shape=(1, env.state_size), name='body')
-        self.log_model(policy.head, device, input_shape=(env.action_size, fc_neurons), name='head')
+        self.log_model(policy.head, device, input_shape=(env.action_size, params['fc_neurons']), name='head')
 
         t = trange(self.params['num_iterations'])
         try:
@@ -119,7 +93,8 @@ class AnilTRPO(Experiment):
                 iter_policies = []
 
                 task_list = env.sample_tasks(self.params['meta_batch_size'])
-                for task_i in trange(len(task_list), leave=False, desc="Task", position=0):
+
+                for task_i in trange(len(task_list), leave=False, desc='Task', position=0):
                     task = task_list[task_i]
 
                     clone = deepcopy(policy)
@@ -128,22 +103,23 @@ class AnilTRPO(Experiment):
                     task = ch.envs.Runner(env)
 
                     # Fast adapt
-                    learner, task_replay, task_rew = fast_adapt_trpo(task, clone, baseline, self.params, anil=True,
+                    learner, task_replay, task_rew = fast_adapt_trpo(task, clone, baseline, self.params,
+                                                                     anil=True,
                                                                      first_order=True, device=device)
 
                     iter_reward += task_rew
                     iter_replays.append(task_replay)
                     iter_policies.append(learner)
 
-                # Meta-optimize
-                meta_optimize_trpo(self.params, policy, baseline, iter_replays, iter_policies, device)
-
                 # Log
-                adapt_reward = iter_reward / self.params['meta_batch_size']
-                metrics = {'adapt_reward': adapt_reward}
+                average_return = iter_reward / self.params['meta_batch_size']
+                metrics = {'average_return': average_return}
 
                 t.set_postfix(metrics)
                 self.log_metrics(metrics)
+
+                # Meta-optimize
+                meta_optimize_trpo(self.params, policy, baseline, iter_replays, iter_policies, device)
 
                 if iteration % self.params['save_every'] == 0:
                     self.save_model_checkpoint(policy.body, 'body_' + str(iteration + 1))
@@ -162,13 +138,14 @@ class AnilTRPO(Experiment):
 
         self.logger['elapsed_time'] = str(round(t.format_dict['elapsed'], 2)) + ' sec'
         # Evaluate on new test tasks
+        env = make_env(env_name, workers, params['seed'], test=True)
         self.logger['test_reward'] = evaluate_trpo(env, policy, baseline, eval_params)
         self.log_metrics({'test_reward': self.logger['test_reward']})
         self.save_logs_to_file()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ANIL-TRPO on RL-MuJoCo tasks')
+    parser = argparse.ArgumentParser(description='ANIL-TRPO on RL tasks')
 
     parser.add_argument('--env', type=str, default=env_name, help='Pick an environment')
 
