2e2bacf4dc1751a9ed4d43142af8a61b0b9c6683
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-05-13 10:56:26 +0200

    Refactor RL adapt function name

--- core_functions/__init__.py
+++ core_functions/__init__.py
@@ -2,4 +2,4 @@
 
 from .vision import fast_adapt, accuracy, prepare_batch
 from .policies import DiagNormalPolicy, DiagNormalPolicyCNN, BaselineCNN, ANILDiagNormalPolicy
-from .rl import adapt_trpo_a2c, meta_surrogate_loss, meta_optimize, evaluate
+from .rl import fast_adapt_trpo_a2c, meta_surrogate_loss, meta_optimize, evaluate
