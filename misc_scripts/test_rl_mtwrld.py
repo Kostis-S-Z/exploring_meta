c71338f1b8e384f8242b97f18f14085bb0c9ace2
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-05-15 13:03:29 +0200

    Update testing

--- misc_scripts/test_rl_mtwrld.py
+++ misc_scripts/test_rl_mtwrld.py
@@ -7,18 +7,20 @@ import json
 
 import torch
 import cherry as ch
-import learn2learn as l2l
+from learn2learn.algorithms import MAML
 
-import utils
+from utils import make_env
 from misc_scripts import run_cl_rl_exp
 from core_functions.rl import evaluate
 from core_functions.policies import DiagNormalPolicy
 
 base = '/home/kosz/Projects/KTH/Thesis/exploring_meta/rl/results/'
-model_path = 'maml_ppo_ML1_push-v1_15_05_10h31_42_1472'
+model_path = 'maml_ppo_ML1_push-v1_15_05_11h29_42_7678'
+checkpoint = None  # or choose a number
 path = base + model_path
-ANIL = False
-ALGO = model_path.split('_')[1]
+ML_ALGO = model_path.split('_')[0]
+RL_ALGO = model_path.split('_')[1]
+DATASET = model_path.split('_')[2] + '_' + model_path.split('_')[3]
 
 render = True
 
@@ -30,7 +32,7 @@ workers = 1
 cuda = False
 
 # An episode can have either a finite number of steps, e.g 100 for Particles 2D or until done
-
+anil = False if ML_ALGO == 'maml' else True
 eval_params = {
     'adapt_steps': 2,  # Number of steps to adapt to a new task
     'adapt_batch_size': 5,  # Number of shots per task
@@ -50,6 +52,7 @@ cl_params = {
 
 def run():
     # Initialize
+    print(f'Testing {ML_ALGO}-{RL_ALGO} on {DATASET}')
     try:
         with open(path + '/logger.json', 'r') as f:
             params = json.load(f)['config']
@@ -57,29 +60,27 @@ def run():
         print('WARNING CONFIG NOT FOUND. Using default parameters')
         params = dict()
         params['inner_lr'] = 0.1
+        params['ppo_epochs'] = 2
+        params['ppo_clip_ratio'] = 0.1
         params['tau'] = 1.0
         params['gamma'] = 0.99
         params['seed'] = 42
 
     device = torch.device('cpu')
-
     random.seed(params['seed'])
     np.random.seed(params['seed'])
     torch.manual_seed(params['seed'])
 
-    env = make_env('ML1', 'pick-place-v1', params['seed'])
+    env = make_env(DATASET, workers, params['seed'], test=True)
 
-    if cuda and torch.cuda.device_count():
-        print(f'Running on {torch.cuda.get_device_name(0)}')
-        torch.cuda.manual_seed(params['seed'])
-        device = torch.device('cuda')
+    eval_params.update(params)
 
-    eval_params['inner_lr'] = params['inner_lr']
-    eval_params['tau'] = params['tau']
-    eval_params['gamma'] = params['gamma']
-
-    policy_path = path + '/model_checkpoints/model_101.pt'
-    baseline_path = path + '/model_checkpoints/model_baseline_101.pt'
+    if checkpoint is None:
+        policy_path = path + '/model.pt'
+        baseline_path = path + '/baseline.pt'
+    else:
+        policy_path = path + f'/model_checkpoints/model_{checkpoint}.pt'
+        baseline_path = path + f'/model_checkpoints/model_baseline_{checkpoint}.pt'
 
     baseline = ch.models.robotics.LinearValue(env.state_size, env.action_size)
     baseline.load_state_dict(torch.load(baseline_path))
@@ -87,36 +88,17 @@ def run():
 
     policy = DiagNormalPolicy(env.state_size, env.action_size)
     policy.load_state_dict(torch.load(policy_path))
+    policy = MAML(policy, lr=eval_params['inner_lr'])
     policy.to(device)
 
     if evaluate_model:
-        eval_reward = evaluate(ALGO, env, policy, baseline, eval_params, anil=ANIL, render=render)
+        eval_reward = evaluate(RL_ALGO, env, policy, baseline, eval_params, anil=anil, render=render)
         print(eval_reward)
 
     # Run a Continual Learning experiment
     if cl_exp:
         print('Running Continual Learning experiment...')
         run_cl_rl_exp(path, env, policy, baseline, cl_params=cl_params)
-
-
-def make_env(benchmark, task, seed, test=False):
-    benchmark_env = getattr(utils, f'MetaWorld{benchmark}')  # Use modded MetaWorld env
-
-    def init_env():
-        if test:
-            env = benchmark_env.get_test_tasks(task)
-        else:
-            env = benchmark_env.get_train_tasks(task)
-
-        env = ch.envs.ActionSpaceScaler(env)
-        return env
-
-    env = l2l.gym.AsyncVectorEnv([init_env for _ in range(workers)])
-
-    env.seed(seed)
-    env.set_task(env.sample_tasks(1)[0])
-    env = ch.envs.Torch(env)
-    return env
 
 
 if __name__ == '__main__':
