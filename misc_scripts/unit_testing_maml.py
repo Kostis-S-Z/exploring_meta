6ad37b80cca336bec0fd8c1f221002e2e4ad19ac
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-06-03 15:52:50 +0200

    Additions to maml testing

--- misc_scripts/unit_testing_maml.py
+++ misc_scripts/unit_testing_maml.py
@@ -7,18 +7,23 @@ import numpy as np
 from copy import deepcopy
 
 from tqdm import trange, tqdm
+from torchsummary import summary_string
 
 import cherry as ch
+from cherry.algorithms import a2c, trpo, ppo
 from learn2learn.algorithms import MAML
 
 from utils import *
 from core_functions.policies import DiagNormalPolicyANIL
-from core_functions.rl import fast_adapt_ppo, evaluate_ppo, set_device
+from core_functions.rl import fast_adapt_ppo, evaluate_ppo, set_device, get_episode_values
 
+Layer1 = 2
+Layer2 = 4
+HEAD = 5
 #   - ML1_reach-v1, ML1_pick-place-v1, ML1_push-v1
 #   - ML10, ML45
 env_name = 'ML1_push-v1'
-workers = 5
+workers = 2
 
 params = {
     # Inner loop parameters
@@ -26,9 +31,9 @@ params = {
     'ppo_clip_ratio': 0.1,
     'inner_lr': 0.05,
     'adapt_steps': 1,
-    'adapt_batch_size': 10,  # 'shots' (will be *evenly* distributed across workers)
+    'adapt_batch_size': 4,  # 'shots' (will be *evenly* distributed across workers)
     # Outer loop parameters
-    'meta_batch_size': 20,  # 'ways'
+    'meta_batch_size': 8,  # 'ways'
     'outer_lr': 0.1,
     # Common parameters
     'activation': 'tanh',  # for MetaWorld use tanh, others relu
@@ -60,7 +65,10 @@ def main():
     policy = MAML(policy, lr=params['inner_lr'])
     meta_optimizer = torch.optim.Adam(policy.parameters(), lr=params['outer_lr'])
 
-    test_maml_ppo_inner_loop(task, policy, baseline)
+    model_info, _ = summary_string(policy, (1, env.state_size), device=device)
+    print(model_info)
+
+    # test_maml_ppo_inner_loop(task, policy, baseline)
 
     test_maml_ppo_outer_loop(env, policy, baseline, meta_optimizer)
 
@@ -135,17 +143,17 @@ def test_maml_ppo_outer_loop(env, policy, baseline, meta_optimizer):
     print(f"FEAT 1 : {features_1_change}")
 
 
-def fast_adapt_ppo(task, learner, baseline, params, anil=False, render=False):
+def fast_adapt_ppo(task, learner, baseline, prms, anil=False, render=False):
     # During inner loop adaptation we do not store gradients for the network body
     if anil:
         learner.module.turn_off_body_grads()
 
-    for step in range(params['adapt_steps']):
+    for step in range(prms['adapt_steps']):
         # Collect adaptation / support episodes
-        support_episodes = task.run(learner, episodes=params['adapt_batch_size'], render=render)
+        support_episodes = task.run(learner, episodes=prms['adapt_batch_size'], render=render)
 
         # Calculate loss & fit the value function & update the policy
-        inner_loss = ppo_update(support_episodes, learner, baseline, params, anil=anil)
+        inner_loss = ppo_update(support_episodes, learner, baseline, prms, anil=anil)
         # print(f'Inner loss {step}: {round(inner_loss.item(), 3)}')
 
     # We need to include the body network parameters for the query set
@@ -153,15 +161,58 @@ def fast_adapt_ppo(task, learner, baseline, params, anil=False, render=False):
         learner.module.turn_on_body_grads()
 
     # Collect evaluation / query episodes
-    query_episodes = task.run(learner, episodes=params['adapt_batch_size'])
+    query_episodes = task.run(learner, episodes=prms['adapt_batch_size'])
     # Calculate loss for the outer loop optimization WITHOUT adapting (works, tested!)
     eval_learner = learner.clone()
     eval_baseline = deepcopy(baseline)
     outer_loss = ppo_update(query_episodes, eval_learner, eval_baseline, params, anil=anil)
     # Calculate the average reward of the evaluation episodes
-    query_rew = query_episodes.reward().sum().item() / params['adapt_batch_size']
+    query_rew = query_episodes.reward().sum().item() / prms['adapt_batch_size']
 
     return outer_loss, query_rew
+
+
+def ppo_update(episodes, learner, baseline, params, anil=False):
+    """
+    Inspired by cherry-rl/examples/spinning-up/cherry_ppo.py
+    """
+
+    # Get values to device
+    states, actions, rewards, dones, next_states = get_episode_values(episodes)
+
+    returns = ch.td.discount(params['gamma'], rewards, dones)
+
+    # Update value function and get new values
+    baseline.fit(states, returns)
+    values = baseline(states)
+    new_values = baseline(next_states)
+    bootstraps = values * (1.0 - dones) + new_values * dones
+
+    # Compute advantages
+    with torch.no_grad():
+        advantages = ch.pg.generalized_advantage(gamma=params['gamma'], tau=params['tau'],
+                                                 rewards=rewards, dones=dones, values=bootstraps,
+                                                 next_value=torch.zeros(1, device=values.device))
+        advantages = ch.normalize(advantages, epsilon=1e-8).detach()
+        # Calculate loss between states and action in the network
+        old_log_probs = learner.log_prob(states, actions)
+
+    # Initialize inner loop PPO optimizer
+    av_loss = 0.0
+
+    for ppo_epoch in range(params['ppo_epochs']):
+        new_log_probs = learner.log_prob(states, actions)
+
+        # Compute the policy loss
+        loss = ppo.policy_loss(new_log_probs, old_log_probs, advantages, clip=params['ppo_clip_ratio'])
+
+        # Adapt model based on the loss
+        learner.adapt(loss, allow_unused=anil)
+
+        # baseline.fit(states, returns)  # TODO: update the value function in every epoch? only once outside?
+        av_loss += loss
+
+    return av_loss / params['ppo_epochs']
 
 
 if __name__ == '__main__':
