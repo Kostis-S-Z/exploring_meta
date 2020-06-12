ddbe52a81b5ccaaf82eeaee9c754f275e71d89b5
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-06-12 18:43:35 +0200

    Add baselines evaluation method

--- baselines/ppo.py
+++ baselines/ppo.py
@@ -8,11 +8,13 @@ import numpy as np
 from tqdm import trange
 
 import cherry as ch
+from learn2learn.algorithms import MAML
+
 from cherry.algorithms import ppo
 
 from utils import *
 from core_functions.policies import DiagNormalPolicy
-from core_functions.rl import get_episode_values, compute_advantages
+from core_functions.rl import get_episode_values, compute_advantages, evaluate_ppo
 
 
 params = {
@@ -30,7 +32,12 @@ params = {
     # Other parameters
     'num_iterations': 1000,
     'save_every': 25,
-    'seed': 42}
+    'seed': 42,
+    # For evaluation
+    'n_tasks': 10,
+    'adapt_steps': 3,
+    'adapt_batch_size': 20,
+    'inner_lr': 0.1}
 
 
 # Environments:
@@ -117,7 +124,8 @@ class PPO(Experiment):
 
         self.logger['elapsed_time'] = str(round(t.format_dict['elapsed'], 2)) + ' sec'
         # Evaluate on new test tasks
-        env = make_env(env_name, workers, params['seed'], test=True)
+        policy = MAML(policy, lr=self.params['inner_lr'])
+        self.logger['test_reward'] = evaluate_ppo(env_name, policy, baseline, params)
         self.log_metrics({'test_reward': self.logger['test_reward']})
         self.save_logs_to_file()
 
